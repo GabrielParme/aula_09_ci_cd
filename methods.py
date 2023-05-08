0c0e314e6f118d914653e598922205c8d46f8a03
Gabriel Parmigiano <parmigianogabriel@gmail.com>
2023-05-08 08:53:10 -0300

    metodos

--- methods.py
+++ methods.py
@@ -5,3 +5,19 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def sum(num1, num2):
+    sum = num1 + num2
+    return sum
+
+def subtract(num1, num2):
+    difference = num1 - num2
+    return difference
+
+def multiply(num1, num2):
+    product = num1 * num2
+    return product
+
+def division(num1, num2):
+    quotient = num1 / num2
+    return quotient
