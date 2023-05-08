cdfb6b24e99c487a8bd88e4ebccc2863f5597568
Gabriel Parmigiano <parmigianogabriel@gmail.com>
2023-05-08 08:48:24 -0300

    testes

--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,49 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_sum():
+    # given two numbers
+    num1 = 2
+    num2 = 5
+
+    # when we calculate the sum
+    output = methods.sum(num1, num2)
+
+    #then the sum shoult be 7
+    assert output == 7
+
+def test_subtract():
+    #given two numbers
+    num1 = 5
+    num2 = 2
+
+    #when we calculate the difference
+    output = methods.subtract(num1, num2)
+
+    #then the difference should be 3
+    assert output == 3
+
+def test_multiply():
+    #given two numbers
+    num1 = 5
+    num2 = 2
+
+    #when we calculate the product 
+    output = methods.multiply(num1, num2)
+
+    #then the product should be 10
+    assert output == 10
+
+def test_division():
+    ##given two numbers
+    num1 = 5
+    num2 = 2
+
+    #when we calculate the quotient 
+    output = methods.division(num1, num2)
+
+    #then the quotient should be 2.5
+    assert output == 2.5
+    
+    
